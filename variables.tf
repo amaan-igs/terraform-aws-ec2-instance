cf5b338d91f52c7b22fa5203647d007a4b3895db
Amaan Ul Haq Siddiqui <amaanulhaq.s@outlook.com>
2025-02-11 20:41:53 +0500

    refactor: remove default value for IAM instance profile variable

--- variables.tf
+++ variables.tf
@@ -61,7 +61,6 @@ variable "tags" {
 variable "iam_instance_profile" {
   description = "The IAM instance profile to attach to the EC2 instance."
   type        = string
-  default     = "" # Optional
 }
 
 variable "ebs_volumes" {
