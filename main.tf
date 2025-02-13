e0ca3269c1e075eee5513251fb1aab565a6b2487
Amaan Ul Haq Siddiqui <amaanulhaq.s@outlook.com>
2025-02-13 22:19:32 +0500

    feat: add Terraform lock file and enhance EC2 instance configuration with monitoring and EBS optimization

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
 resource "aws_instance" "ec2_instance" {
-  ami                    = var.ami_id # Required
-  instance_type          = var.instance_type # Optional (default: t2.micro)
-  subnet_id              = var.subnet_id # Required
-  vpc_security_group_ids = var.security_group_ids # Required
-  key_name               = var.key_name # Optional
+  ami                    = var.ami_id                                                       # Required
+  instance_type          = var.instance_type                                                # Optional (default: t2.micro)
+  subnet_id              = var.subnet_id                                                    # Required
+  vpc_security_group_ids = var.security_group_ids                                           # Required
+  key_name               = var.key_name                                                     # Optional
   iam_instance_profile   = var.iam_instance_profile != "" ? var.iam_instance_profile : null # Optional
-  tags                   = merge(var.tags, { Name = var.instance_name }) # Required (instance_name), Optional (tags)
-  monitoring = true #CKV_AWS_126
-  ebs_optimized = true #CKV_AWS_135
+  tags                   = merge(var.tags, { Name = var.instance_name })                    # Required (instance_name), Optional (tags)
+  monitoring             = true                                                             #CKV_AWS_126
+  ebs_optimized          = true                                                             #CKV_AWS_135
 
   # Root volume configuration
   root_block_device {
-    device_name = ebs_block_device.value.device_name
-    volume_size = var.root_volume_size # Optional (default: 8)
-    volume_type = var.root_volume_type # Optional (default: gp2)
-    encrypted   = var.root_volume_encryption # Optional (default: true)
+    device_name = "/dev/sda1" # Change this to the correct root volume device name
+    volume_size = var.root_volume_size
+    volume_type = var.root_volume_type
+    encrypted   = var.root_volume_encryption
   }
 
-  # Additional EBS volumes
+
   dynamic "ebs_block_device" {
     for_each = var.ebs_volumes
     content {
-      device_name = ebs_block_device.value.device_name
-      volume_size = ebs_block_device.value.volume_size
-      volume_type = ebs_block_device.value.volume_type
-      encrypted   = ebs_block_device.value.encrypted
+      device_name = each.value.device_name
+      volume_size = each.value.volume_size
+      volume_type = each.value.volume_type
+      encrypted   = each.value.encrypted
     }
   }
 
